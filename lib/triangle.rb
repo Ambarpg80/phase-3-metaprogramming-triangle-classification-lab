bb2cd68a85687e06587b2cefb13c054119bb89b5
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-03-17 19:02:39 -0400

    commit tree bece7e480dcad3f909ded0fa41f6df068b5c05b8

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :equilateral, :isosceles, :scalene, :sides
+
+  def initialize(a , b, c)
+    @sides = [a , b , c].sort  
+  end
+
+  def is_triangle?()
+    ( (@sides.all? {|side| side > 0} )  && ( (@sides[0] + @sides[1]) > @sides[2] ) )
+    
+  end
+
+  def type_of_triangle()
+    if ((@sides[0] == @sides[1]) && (@sides[1] == @sides[2]))
+    return :equilateral 
+    elsif ( (@sides[0] == @sides[1]) || (@sides[1] == @sides[2] ) || (@sides[0] == @sides[2]) )
+    return :isosceles
+    else
+    return  :scalene
+    end
+  end
+
+  def kind()
+    if is_triangle?
+      return type_of_triangle 
+    else 
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
